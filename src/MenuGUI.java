a08347d16fb9af9767f07c7ac5ef86b99fd2f9d2
LordTr1gg3r <simonthow1996@gmail.com>
2019-11-02 15:53:45 +0000

    Structure implemeted
    Structure for project implmented. TODO add menu and panels to program

---
+++ src/MenuGUI.java
@@ -0,0 +1,15 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.*;
+
+
+class MenuGUI extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+}
